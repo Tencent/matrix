a7976643f88b45a704a5543155935f8f24dddece
felixzhou <wx_4f005dbe653d45bba623b7e688e256e0@git.code.tencent.com>
2018-12-28 21:18:32 +0800

    style check and generate sourcejar

--- matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/UnusedResourcesTask.java
+++ matrix/matrix-android/matrix-apk-canary/src/main/java/com/tencent/matrix/apk/model/task/UnusedResourcesTask.java
@@ -41,10 +41,8 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
