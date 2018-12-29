dd955e4fb0e62f8860beff6238762973bb038fb2
felixzhou <wx_4f005dbe653d45bba623b7e688e256e0@git.code.tencent.com>
2018-12-29 12:12:15 +0800

    add feature: battery , thread and meminfo
    sqlitelint remove abi  x86_64 and arm64-v8a first

---
+++ matrix/matrix-android/matrix-thread-canary/src/main/java/com/tencent/matrix/threadcanary/Constants.java
@@ -0,0 +1,15 @@
+package com.tencent.matrix.threadcanary;
+
+public class Constants {
+    public static final String TAG_PLUGIN = "Thread";
+    public static final String REPORT_KEY_THREAD_COUNT = "thread_count";
+    public static final String REPORT_KEY_THREAD_LIST = "thread_list";
+    public static final String REPORT_KEY_THREAD_IS_SYSTEM = "thread_info_is_system";
+    public static final String REPORT_KEY_THREAD_INFO_NAME = "thread_info_name";
+    public static final String REPORT_KEY_THREAD_INFO_TID = "thread_info_tid";
+    public static final String REPORT_KEY_THREAD_INFO_STATE = "thread_info_state";
+    public static final String REPORT_KEY_THREAD_INFO_JIFFIES = "thread_info_jiffies";
+    public static final String REPORT_KEY_THREAD_INFO_BIG_STACK_SIZE = "thread_info_big_stack_size";
+    public static final String REPORT_KEY_THREAD_INFO_BIG_VIRTUAL_SIZE = "thread_info_big_virtual_size";
+
+}
