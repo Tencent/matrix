00a780a5d8f9172a2337677a549cd29245af724f
felixzhou <wx_4f005dbe653d45bba623b7e688e256e0@git.code.tencent.com>
2018-12-29 15:33:37 +0800

    add codecc build config

--- codecc.sh
+++ codecc.sh
@@ -1,4 +1,6 @@
+
 cd /home/android/hudson/workspace/open-check/build/repositories/matrix-android/matrix/matrix-android
+
 ./gradlew clean compileDebugJavaWithJavac -PforCoverity --no-daemon
 ./gradlew :matrix-io-canary:build
 ./gradlew :matrix-sqlite-lint:build
