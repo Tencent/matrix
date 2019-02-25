f12d4dda7d1b85897af8a902bbce3e4c61120850
tom-ystang <yinshengtang92@gmail.com>
2019-02-25 14:18:39 +0800

    fix SecurityException when startService in MatrixJobIntentService. (#120)
    * [IOCanary][bug-fix]CloseGuardHooker.tryHook没有适配9.0 #86
    
    * fix FrameTracer doFrame bug
    
    # Conflicts:
    #	matrix/matrix-android/matrix-trace-canary/src/main/java/com/tencent/matrix/trace/tracer/FrameTracer.java
    
    * fix MethodTracer trace exception
    
    * bugfix: protect startService invocation in MatrixJobIntentService in case system throw an exception in some edge case.

--- matrix/matrix-android/matrix-resource-canary/matrix-resource-canary-android/src/main/java/com/tencent/matrix/resource/MatrixJobIntentService.java
+++ matrix/matrix-android/matrix-resource-canary/matrix-resource-canary-android/src/main/java/com/tencent/matrix/resource/MatrixJobIntentService.java
@@ -193,20 +193,24 @@ public abstract class MatrixJobIntentService extends Service {
         void enqueueWork(Intent work) {
             Intent intent = new Intent(work);
             intent.setComponent(mComponentName);
-            if (mContext.startService(intent) != null) {
-                synchronized (this) {
-                    if (!mLaunchingService) {
-                        mLaunchingService = true;
-                        if (!mServiceProcessing && mLaunchWakeLock != null) {
-                            // If the service is not already holding the wake lock for
-                            // itself, acquire it now to keep the system running until
-                            // we get this work dispatched.  We use a timeout here to
-                            // protect against whatever problem may cause it to not get
-                            // the work.
-                            mLaunchWakeLock.acquire(60 * 1000);
+            try {
+                if (mContext.startService(intent) != null) {
+                    synchronized (this) {
+                        if (!mLaunchingService) {
+                            mLaunchingService = true;
+                            if (!mServiceProcessing && mLaunchWakeLock != null) {
+                                // If the service is not already holding the wake lock for
+                                // itself, acquire it now to keep the system running until
+                                // we get this work dispatched.  We use a timeout here to
+                                // protect against whatever problem may cause it to not get
+                                // the work.
+                                mLaunchWakeLock.acquire(60 * 1000);
+                            }
                         }
                     }
                 }
+            } catch (Throwable thr) {
+                MatrixLog.printErrStackTrace(TAG, thr, "Exception occurred.");
             }
         }
 
