7c5941e392436690ada2787bce7feaff63771c4c
changelcai <changelcai@tencent.com>
2019-07-05 11:26:45 +0800

    fix TraceDataUtils bug

--- matrix/matrix-android/matrix-trace-canary/src/main/java/com/tencent/matrix/trace/constants/Constants.java
+++ matrix/matrix-android/matrix-trace-canary/src/main/java/com/tencent/matrix/trace/constants/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
     public static final int FILTER_STACK_MAX_COUNT = 60;
     public static final float FILTER_STACK_KEY_ALL_PERCENT = .3F;
     public static final float FILTER_STACK_KEY_PATENT_PERCENT = .8F;
-    public static final int DEFAULT_EVIL_METHOD_THRESHOLD_MS = 1000;
+    public static final int DEFAULT_EVIL_METHOD_THRESHOLD_MS = 700;
     public static final int DEFAULT_FPS_TIME_SLICE_ALIVE_MS = 10 * 1000;
     public static final int TIME_MILLIS_TO_NANO = 1000000;
     public static final int DEFAULT_ANR = 5 * 1000;
