19241591af0825f1100f8a1bd767ba887420c7e3
yvesluo <yvesluo@tencent.com>
2019-07-22 21:40:31 +0800

    add FDInfoReader

---
+++ wxperf/src/main/java/com/tencent/mm/performance/fd/FDDumpBridge.java
@@ -0,0 +1,11 @@
+package com.tencent.mm.performance.fd;
+
+/**
+ * Created by Yves on 2019-07-22
+ */
+public class FDDumpBridge {
+    static {
+        System.loadLibrary("wxperf");
+    }
+    public static native String getFdPathName(String path);
+}
