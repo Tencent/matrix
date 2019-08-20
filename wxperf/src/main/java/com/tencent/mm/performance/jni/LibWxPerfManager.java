9a98685633903cb3b820a1d2f991dc677127f38e
yvesluo <yvesluo@tencent.com>
2019-08-20 16:15:19 +0800

    loadLibrary 增加 try-catch 保护

---
+++ wxperf/src/main/java/com/tencent/mm/performance/jni/LibWxPerfManager.java
@@ -0,0 +1,26 @@
+package com.tencent.mm.performance.jni;
+
+/**
+ * Created by Yves on 2019-08-20
+ */
+public class LibWxPerfManager {
+
+    public static final LibWxPerfManager INSTANCE = new LibWxPerfManager();
+
+    private volatile boolean isLibLoaded = false;
+
+    public void init() {
+        try {
+            if (!isLibLoaded) {
+                System.loadLibrary("wxperf");
+                isLibLoaded = true;
+            }
+        } catch (Exception e) {
+            isLibLoaded = false;
+        }
+    }
+
+    public boolean initOk() {
+        return isLibLoaded;
+    }
+}
--- wxperf/src/main/java/com/tencent/mm/performance/jni/fd/FDDumpBridge.java
+++ wxperf/src/main/java/com/tencent/mm/performance/jni/fd/FDDumpBridge.java
@@ -1,11 +1,22 @@
 package com.tencent.mm.performance.jni.fd;
 
+import com.tencent.mm.performance.jni.LibWxPerfManager;
+
 /**
  * Created by Yves on 2019-07-22
  */
 public class FDDumpBridge {
+
     static {
-        System.loadLibrary("wxperf");
+        LibWxPerfManager.INSTANCE.init();
     }
-    public static native String getFdPathName(String path);
+
+    public static String getFdPathName(String path) {
+        if (!LibWxPerfManager.INSTANCE.initOk()) {
+            return null;
+        }
+        return getFdPathNameNative(path);
+    }
+
+    private static native String getFdPathNameNative(String path);
 }
