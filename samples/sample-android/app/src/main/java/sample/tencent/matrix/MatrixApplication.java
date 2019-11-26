5e9e7217d649d7d632fcd4bbd5869e571b94b57b
txfelixzhou <felixzhou@tencent.com>
2019-11-26 16:38:52 +0800

    merge from feature/dev and upgrade to 0.6.5

--- samples/sample-android/app/src/main/java/sample/tencent/matrix/MatrixApplication.java
+++ samples/sample-android/app/src/main/java/sample/tencent/matrix/MatrixApplication.java
@@ -18,6 +18,7 @@ package sample.tencent.matrix;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 
 import com.tencent.matrix.Matrix;
 import com.tencent.matrix.iocanary.IOCanaryPlugin;
@@ -26,7 +27,6 @@ import com.tencent.matrix.resource.ResourcePlugin;
 import com.tencent.matrix.resource.config.ResourceConfig;
 import com.tencent.matrix.trace.TracePlugin;
 import com.tencent.matrix.trace.config.TraceConfig;
-import com.tencent.matrix.util.MatrixHandlerThread;
 import com.tencent.matrix.util.MatrixLog;
 import com.tencent.sqlitelint.SQLiteLint;
 import com.tencent.sqlitelint.SQLiteLintPlugin;
@@ -91,11 +91,17 @@ public class MatrixApplication extends Application {
         if (matrixEnable) {
 
             //resource
-            builder.plugin(new ResourcePlugin(new ResourceConfig.Builder()
+            Intent intent = new Intent();
+            ResourceConfig.DumpMode mode = ResourceConfig.DumpMode.AUTO_DUMP;
+            MatrixLog.i(TAG, "Dump Activity Leak Mode=%s", mode);
+            intent.setClassName(this.getPackageName(), "com.tencent.mm.ui.matrix.ManualDumpActivity");
+            ResourceConfig resourceConfig = new ResourceConfig.Builder()
                     .dynamicConfig(dynamicConfig)
-                    .setAutoDumpHprofMode(ResourceConfig.DumpMode.MANUAL_DUMP)
-                    .setDetectDebuger(true)     //only set true when in sample, not in your app
-                    .build()));
+                    .setAutoDumpHprofMode(mode)
+//                .setDetectDebuger(true) //matrix test code
+                    .setNotificationContentIntent(intent)
+                    .build();
+            builder.plugin(new ResourcePlugin(resourceConfig));
             ResourcePlugin.activityLeakFixer(this);
 
             //io
@@ -107,16 +113,13 @@ public class MatrixApplication extends Application {
 
             // prevent api 19 UnsatisfiedLinkError
             //sqlite
-            SQLiteLintConfig config = initSQLiteLintConfig();
-            SQLiteLintPlugin sqLiteLintPlugin = new SQLiteLintPlugin(config);
-            builder.plugin(sqLiteLintPlugin);
-
-            MatrixHandlerThread.getDefaultHandler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    MatrixHandlerThread.getDefaultHandler().postDelayed(this, 200);
-                }
-            }, 2000);
+            SQLiteLintConfig sqlLiteConfig;
+            try {
+                sqlLiteConfig = new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.CUSTOM_NOTIFY);
+            } catch (Throwable t) {
+                sqlLiteConfig = new SQLiteLintConfig(SQLiteLint.SqlExecutionCallbackMode.CUSTOM_NOTIFY);
+            }
+            builder.plugin(new SQLiteLintPlugin(sqlLiteConfig));
         }
 
         Matrix.init(builder.build());
