caa3e0e312d4f701d15ec876b722a318e111d64b
williamjin <wiliamjin@tencent.com>
2019-12-05 15:43:12 +0800

    fix ignoreResources

--- matrix/matrix-android/matrix-gradle-plugin/src/main/groovy/com/tencent/matrix/plugin/task/RemoveUnusedResourcesTask.groovy
+++ matrix/matrix-android/matrix-gradle-plugin/src/main/groovy/com/tencent/matrix/plugin/task/RemoveUnusedResourcesTask.groovy
@@ -224,6 +224,18 @@ public class RemoveUnusedResourcesTask extends DefaultTask {
             commandList.add("--rTxt")
             commandList.add(rTxtFile)
 
+            StringBuilder ignoreResParam = new StringBuilder()
+            Set<String> ignoreResources = project.extensions.matrix.removeUnusedResources.ignoreResources
+            if (!ignoreResources.isEmpty()) {
+                for (String ignore : ignoreResources) {
+                    ignoreResParam.append(ignore)
+                    ignoreResParam.append(',')
+                }
+                ignoreResParam.deleteCharAt(ignoreResParam.length() - 1)
+                commandList.add("--ignoreResources")
+                commandList.add(ignoreResParam.toString())
+            }
+
             if (project.extensions.matrix.removeUnusedResources.deDuplicate) {
                 commandList.add("-duplicatedFile")
             }
@@ -474,7 +486,7 @@ public class RemoveUnusedResourcesTask extends DefaultTask {
                             Log.w(TAG, "parse entry %s resource name failed!", zipEntry.name)
                         }
                     } else {
-                        if (!zipEntry.name.startsWith("META-INF/") && !zipEntry.name.equals(ARSC_FILE_NAME)) {
+                        if ( (!zipEntry.name.startsWith("META-INF/") || (!zipEntry.name.endsWith(".SF") &&  !zipEntry.name.endsWith(".MF") && !zipEntry.name.endsWith(".RSA"))) && !zipEntry.name.equals(ARSC_FILE_NAME)) {
                             if (zipEntry.getMethod() == ZipEntry.DEFLATED) {
                                 compressedEntry.add(destFile)
                             }
