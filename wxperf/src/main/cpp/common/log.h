19011bdfe4624791281ad804d21ea616e79d78f9
yvesluo <yvesluo@tencent.com>
2020-04-23 21:55:49 +0800

    disable log

--- wxperf/src/main/cpp/common/log.h
+++ wxperf/src/main/cpp/common/log.h
@@ -7,7 +7,7 @@
 
 #include <android/log.h>
 
-#define LOGD(TAG, FMT, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, FMT, ##args)
+#define LOGD(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_DEBUG, TAG, FMT, ##args)
 #define LOGI(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_INFO, TAG, FMT, ##args)
 #define LOGE(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_ERROR, TAG, FMT, ##args)
 
