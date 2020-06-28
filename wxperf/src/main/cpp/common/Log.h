d55ef29681a8219b5a2fe8c4f1b5c1c80e75dbb7
yvesluo <yvesluo@tencent.com>
2020-06-28 16:15:42 +0800

    disable log

--- wxperf/src/main/cpp/common/Log.h
+++ wxperf/src/main/cpp/common/Log.h
@@ -8,7 +8,7 @@
 #include <time.h>
 #include <android/log.h>
 
-#define LOGD(TAG, FMT, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, FMT, ##args)
+#define LOGD(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_DEBUG, TAG, FMT, ##args)
 #define LOGI(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_INFO, TAG, FMT, ##args)
 #define LOGE(TAG, FMT, args...) //__android_log_print(ANDROID_LOG_ERROR, TAG, FMT, ##args)
 
@@ -34,20 +34,20 @@
 #endif
 
 #define NanoSeconds_Start(timestamp) \
-        struct timespec timestamp; \
-        if (clock_gettime(CLOCK_REALTIME, &timestamp)) { \
-            LOGE(MEMORY_BENCHMARK_TAG, "Err: Get time failed."); \
-        }
+//        struct timespec timestamp; \
+//        if (clock_gettime(CLOCK_REALTIME, &timestamp)) { \
+//            LOGE(MEMORY_BENCHMARK_TAG, "Err: Get time failed."); \
+//        }
 
 #define NanoSeconds_End(result, timestamp) \
-        long long result = 0; \
-        { \
-            struct timespec tms_end; \
-            if (clock_gettime(CLOCK_REALTIME, &tms_end)) { \
-                LOGE(MEMORY_BENCHMARK_TAG, "Err: Get time failed."); \
-            } \
-            result = (tms_end.tv_sec * 1000 * 1000 * 1000 + tms_end.tv_nsec - (timestamp.tv_sec * 1000 * 1000 * 1000 + timestamp.tv_nsec)); \
-        }
+//        long long result = 0; \
+//        { \
+//            struct timespec tms_end; \
+//            if (clock_gettime(CLOCK_REALTIME, &tms_end)) { \
+//                LOGE(MEMORY_BENCHMARK_TAG, "Err: Get time failed."); \
+//            } \
+//            result = (tms_end.tv_sec * 1000 * 1000 * 1000 + tms_end.tv_nsec - (timestamp.tv_sec * 1000 * 1000 * 1000 + timestamp.tv_nsec)); \
+//        }
 
 
 #endif //MEMINFO_LOG_H
