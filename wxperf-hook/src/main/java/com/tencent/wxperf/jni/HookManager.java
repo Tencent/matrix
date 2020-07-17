a01432dc2acb387af001b9568205417ec712b287
yvesluo <yvesluo@tencent.com>
2020-07-17 17:30:46 +0800

    fix so name

--- wxperf-hook/src/main/java/com/tencent/wxperf/jni/HookManager.java
+++ wxperf-hook/src/main/java/com/tencent/wxperf/jni/HookManager.java
@@ -21,7 +21,7 @@ public class HookManager {
 
     private HookManager(){
         try {
-            System.loadLibrary("wxperf");
+            System.loadLibrary("wxperf-jni");
         } catch (Throwable e) {
             Log.printStack(Log.ERROR, TAG, e);
         }
