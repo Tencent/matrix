7ac87f396562e277a2ba69f466b1b966ecf53c28
yvesluo <yvesluo@tencent.com>
2020-08-05 15:24:09 +0800

    get java stacktrace locked

--- wxperf-hook/src/main/cpp/pthread/PthreadHook.cpp
+++ wxperf-hook/src/main/cpp/pthread/PthreadHook.cpp
@@ -160,6 +160,10 @@ static inline bool on_pthread_create_locked(const pthread_t __pthread, char *__j
         m_filtered_pthreads.insert(__pthread);
     }
 
+    if (__java_stacktrace) {
+        get_java_stacktrace(__java_stacktrace, 1024);
+    }
+
     uint64_t native_hash = 0;
     uint64_t java_hash   = 0;
 
@@ -210,10 +214,10 @@ static void on_pthread_create(const pthread_t __pthread) {
     // todo move to on_pthread_create_locked
     const size_t BUF_SIZE         = 1024;
     char         *java_stacktrace = static_cast<char *>(malloc(BUF_SIZE));
-    if (java_stacktrace) {
-        get_java_stacktrace(java_stacktrace, BUF_SIZE);
-//        strncpy(java_stacktrace, " (fake stacktrace)", BUF_SIZE);
-    }
+//    if (java_stacktrace) {
+//        get_java_stacktrace(java_stacktrace, BUF_SIZE);
+////        strncpy(java_stacktrace, " (fake stacktrace)", BUF_SIZE);
+//    }
 //
     LOGD(TAG, "parent_tid: %d -> tid: %d", pthread_gettid_np(pthread_self()), tid);
     bool recorded = on_pthread_create_locked(__pthread, java_stacktrace, tid);
