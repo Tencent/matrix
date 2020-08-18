248e9c4f33f7117143ad0be6a2d05c71917b7bc7
yvesluo <yvesluo@tencent.com>
2020-08-18 14:35:26 +0800

    unwind: lock free

--- wxperf-hook/src/main/cpp/common/Backtrace.cpp
+++ wxperf-hook/src/main/cpp/common/Backtrace.cpp
@@ -66,11 +66,11 @@ namespace wechat_backtrace {
 
     void fp_fast_unwind(uptr *regs, uptr *frames, uptr frameMaxSize, uptr &frameSize) {
 
-        pthread_mutex_lock(&unwind_mutex);
+//        pthread_mutex_lock(&unwind_mutex);
 
         FpUnwind(regs, frames, frameMaxSize, frameSize, false);
 
-        pthread_mutex_unlock(&unwind_mutex);
+//        pthread_mutex_unlock(&unwind_mutex);
     }
 
     void fp_unwind_with_fallback(uptr *regs, uptr *frames, uptr frameMaxSize, uptr &frameSize) {
