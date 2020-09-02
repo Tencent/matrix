08f514f80c917e0b3a823d8de79d55715314f0af
yvesluo <yvesluo@tencent.com>
2020-09-02 21:36:33 +0800

    disable fp unwind for performance

--- wxperf-hook/src/main/cpp/common/StackTrace.cpp
+++ wxperf-hook/src/main/cpp/common/StackTrace.cpp
@@ -97,61 +97,61 @@ static inline void fp_unwind(uptr *frames, size_t max_size, size_t &frame_size) 
 #define MAX_FRAMES 16
 
 void unwind_adapter(std::vector<unwindstack::FrameData> &dst) {
-#ifdef __aarch64__
-
-    uptr frames[MAX_FRAMES];
-    size_t frame_size = 0;
-
-    fp_unwind(frames, MAX_FRAMES, frame_size);
-
-    LOGD("Wxperf.unwind", "unwind adapter: size %zu", frame_size);
-
-    for (auto i = 0; i < frame_size; i++) {
-        unwindstack::FrameData data;
-        data.pc = frames[i];
-        dst.__emplace_back(data);
-    }
-
-    return;
-#endif
-
-#ifdef __arm__
+//#ifdef __aarch64__
+//
+//    uptr frames[MAX_FRAMES];
+//    size_t frame_size = 0;
+//
+//    fp_unwind(frames, MAX_FRAMES, frame_size);
+//
+//    LOGD("Wxperf.unwind", "unwind adapter: size %zu", frame_size);
+//
+//    for (auto i = 0; i < frame_size; i++) {
+//        unwindstack::FrameData data;
+//        data.pc = frames[i];
+//        dst.__emplace_back(data);
+//    }
+//
+//    return;
+//#endif
+//
+//#ifdef __arm__
 
     unwindstack::do_unwind(dst);
 
     return;
-#endif
+//#endif
 
     __android_log_assert("NOT SUPPORT ARCH", "Unwind", "NOT SUPPORT ARCH");
 }
 
 void restore_frame_data(std::vector<unwindstack::FrameData> &frames) {
-#ifdef __aarch64__
+//#ifdef __aarch64__
+//
+//    LOGD("Wxperf.unwind", "restore frame %zu", frames.size());
+//    for (auto& frame_data: frames) {
+//        Dl_info stack_info;
+//        dladdr((void *) frame_data.pc, &stack_info);
+//
+//        frame_data.rel_pc = frame_data.pc - (uptr) stack_info.dli_fbase;
+//        frame_data.function_name = stack_info.dli_sname == nullptr ? "null" : stack_info.dli_sname;
+//        frame_data.map_name = stack_info.dli_fname == nullptr ? "null" : stack_info.dli_fname;
+//    }
+//
+//    return;
+//#endif
+//
+//#ifdef __arm__
 
-    LOGD("Wxperf.unwind", "restore frame %zu", frames.size());
     for (auto& frame_data: frames) {
         Dl_info stack_info;
         dladdr((void *) frame_data.pc, &stack_info);
 
-        frame_data.rel_pc = frame_data.pc - (uptr) stack_info.dli_fbase;
         frame_data.function_name = stack_info.dli_sname == nullptr ? "null" : stack_info.dli_sname;
         frame_data.map_name = stack_info.dli_fname == nullptr ? "null" : stack_info.dli_fname;
     }
 
-    return;
-#endif
-
-#ifdef __arm__
-
-    for (auto& frame_data: frames) {
-        Dl_info stack_info;
-        dladdr((void *) frame_data.pc, &stack_info);
-
-        frame_data.function_name = stack_info.dli_sname == nullptr ? "null" : stack_info.dli_sname;
-        frame_data.map_name = stack_info.dli_fname == nullptr ? "null" : stack_info.dli_fname;
-    }
-
-#endif
+//#endif
     return;
 }
 
