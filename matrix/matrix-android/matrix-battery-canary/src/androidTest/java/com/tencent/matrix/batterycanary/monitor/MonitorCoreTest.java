47eb10681cc21d8b3f4f59f8822d82824c9fa04f
kaedexie <kaedexie@tencent.com>
2020-11-05 17:52:06 +0800

    Update battery with monitor test cases

--- matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/monitor/MonitorCoreTest.java
+++ matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/monitor/MonitorCoreTest.java
@@ -24,6 +24,8 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.tencent.matrix.Matrix;
+import com.tencent.matrix.batterycanary.monitor.feature.AlarmMonitorFeature;
+import com.tencent.matrix.batterycanary.monitor.feature.DeviceStatMonitorFeature;
 import com.tencent.matrix.batterycanary.monitor.feature.JiffiesMonitorFeature;
 import com.tencent.matrix.batterycanary.monitor.feature.LooperTaskMonitorFeature;
 import com.tencent.matrix.batterycanary.monitor.feature.WakeLockMonitorFeature;
@@ -81,10 +83,12 @@ public class MonitorCoreTest {
     @Test
     public void testInitCore() throws InterruptedException {
         BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
-                .enable(JiffiesMonitorFeature.class)
-                .enable(WakeLockMonitorFeature.class)
                 .enable(LooperTaskMonitorFeature.class)
-                .enableBuiltinForegroundNotify(false)
+                .enable(JiffiesMonitorFeature.class)
+                .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
                 .enableForegroundMode(true)
                 .setCallback(spyCallback)
                 .wakelockTimeout(1000)
@@ -97,98 +101,145 @@ public class MonitorCoreTest {
     }
 
     @Test
-    public void testJiffiesFeatures() throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    new Handler(Looper.getMainLooper());
-                }
-            }
-        });
-        thread.setPriority(Thread.MAX_PRIORITY);
-        thread.setName("test-jiffies-thread");
-        thread.start();
-
-        Matrix.init(new Matrix.Builder(((Application) mContext.getApplicationContext())).build());
+    public void testInitCoreWithCustomCfg0() throws InterruptedException {
         BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                .enable(LooperTaskMonitorFeature.class)
                 .enable(JiffiesMonitorFeature.class)
-                .enableBuiltinForegroundNotify(false)
-                .enableForegroundMode(false)
+                .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
                 .setCallback(spyCallback)
-                .wakelockTimeout(1000)
+                .wakelockTimeout(300)
                 .greyJiffiesTime(100)
-                .foregroundLoopCheckTime(1000)
+                .foregroundLoopCheckTime(2000)
                 .build();
 
         final BatteryMonitorCore core = new BatteryMonitorCore(config);
-        mockForegroundSwitching(core);
         core.start();
-        Thread.sleep(10000L);
+        Thread.sleep(1000L);
     }
 
     @Test
-    public void testJiffiesFeaturesWithForegroundLoopCheck() throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    new Handler(Looper.getMainLooper());
-                }
-            }
-        });
-        thread.setPriority(Thread.MAX_PRIORITY);
-        thread.setName("test-jiffies-thread");
-        thread.start();
-
-        Matrix.init(new Matrix.Builder(((Application) mContext.getApplicationContext())).build());
+    public void testInitCoreWithCustomCfg1() throws InterruptedException {
         BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
-                .enable(JiffiesMonitorFeature.class)
-                .enableBuiltinForegroundNotify(false)
+                // .enable(LooperTaskMonitorFeature.class)
+                // .enable(JiffiesMonitorFeature.class)
+                // .enable(DeviceStatMonitorFeature.class)
+                // .enable(WakeLockMonitorFeature.class)
+                // .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
                 .enableForegroundMode(true)
                 .setCallback(spyCallback)
-                .wakelockTimeout(1000)
+                .wakelockTimeout(300)
                 .greyJiffiesTime(100)
-                .foregroundLoopCheckTime(1000)
+                .foregroundLoopCheckTime(2000)
                 .build();
 
         final BatteryMonitorCore core = new BatteryMonitorCore(config);
         core.start();
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                core.onForeground(true);
-            }
-        });
-        Thread.sleep(10000L);
+        Thread.sleep(1000L);
     }
 
-    private void mockForegroundSwitching(final BatteryMonitorCore core) {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                core.onForeground(false);
-            }
-        });
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                core.onForeground(true);
-            }
-        }, 2000L);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                core.onForeground(false);
-            }
-        }, 5000L);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                core.onForeground(true);
-            }
-        }, 9000L);
+    @Test
+    public void testInitCoreWithCustomCfg3() throws InterruptedException {
+        BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                // .enable(LooperTaskMonitorFeature.class)
+                .enable(JiffiesMonitorFeature.class)
+                .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
+                .setCallback(spyCallback)
+                .wakelockTimeout(300)
+                .greyJiffiesTime(100)
+                .foregroundLoopCheckTime(2000)
+                .build();
+
+        final BatteryMonitorCore core = new BatteryMonitorCore(config);
+        core.start();
+        Thread.sleep(1000L);
+    }
+    @Test
+    public void testInitCoreWithCustomCfg4() throws InterruptedException {
+        BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                .enable(LooperTaskMonitorFeature.class)
+                // .enable(JiffiesMonitorFeature.class)
+                .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
+                .setCallback(spyCallback)
+                .wakelockTimeout(300)
+                .greyJiffiesTime(100)
+                .foregroundLoopCheckTime(2000)
+                .build();
+
+        final BatteryMonitorCore core = new BatteryMonitorCore(config);
+        core.start();
+        Thread.sleep(1000L);
+    }
+    @Test
+    public void testInitCoreWithCustomCfg5() throws InterruptedException {
+        BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                .enable(LooperTaskMonitorFeature.class)
+                .enable(JiffiesMonitorFeature.class)
+                // .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
+                .setCallback(spyCallback)
+                .wakelockTimeout(300)
+                .greyJiffiesTime(100)
+                .foregroundLoopCheckTime(2000)
+                .build();
+
+        final BatteryMonitorCore core = new BatteryMonitorCore(config);
+        core.start();
+        Thread.sleep(1000L);
+    }
+    @Test
+    public void testInitCoreWithCustomCfg6() throws InterruptedException {
+        BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                .enable(LooperTaskMonitorFeature.class)
+                .enable(JiffiesMonitorFeature.class)
+                .enable(DeviceStatMonitorFeature.class)
+                // .enable(WakeLockMonitorFeature.class)
+                .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
+                .setCallback(spyCallback)
+                .wakelockTimeout(300)
+                .greyJiffiesTime(100)
+                .foregroundLoopCheckTime(2000)
+                .build();
+
+        final BatteryMonitorCore core = new BatteryMonitorCore(config);
+        core.start();
+        Thread.sleep(1000L);
+    }
+    @Test
+    public void testInitCoreWithCustomCfg7() throws InterruptedException {
+        BatteryMonitorConfig config = new BatteryMonitorConfig.Builder()
+                .enable(LooperTaskMonitorFeature.class)
+                .enable(JiffiesMonitorFeature.class)
+                .enable(DeviceStatMonitorFeature.class)
+                .enable(WakeLockMonitorFeature.class)
+                // .enable(AlarmMonitorFeature.class)
+                .enableBuiltinForegroundNotify(true)
+                .enableForegroundMode(true)
+                .setCallback(spyCallback)
+                .wakelockTimeout(300)
+                .greyJiffiesTime(100)
+                .foregroundLoopCheckTime(2000)
+                .build();
+
+        final BatteryMonitorCore core = new BatteryMonitorCore(config);
+        core.start();
+        Thread.sleep(1000L);
     }
 }
