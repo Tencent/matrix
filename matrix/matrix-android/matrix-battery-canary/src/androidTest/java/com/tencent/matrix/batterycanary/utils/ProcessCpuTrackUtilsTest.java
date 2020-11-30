2b3e458b77aa5d378e3a22e9b16b28bb5488c712
kaedexie <kaedexie@tencent.com>
2020-11-30 20:29:17 +0800

    Fix thread jiffies parsing bugs

--- matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/utils/ProcessCpuTrackUtilsTest.java
+++ matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/utils/ProcessCpuTrackUtilsTest.java
@@ -25,6 +25,7 @@ import android.support.test.runner.AndroidJUnit4;
 import android.text.TextUtils;
 
 import com.tencent.matrix.batterycanary.TestUtils;
+import com.tencent.matrix.batterycanary.utils.BatteryCanaryUtil.ProcStatInfo;
 import com.tencent.matrix.util.MatrixLog;
 import com.tencent.matrix.util.MatrixUtil;
 
@@ -67,7 +68,8 @@ public class ProcessCpuTrackUtilsTest {
     public void testGetCurrentMethodName() {
         Assert.assertEquals(
                 "testGetCurrentMethodName",
-                new Object() {}
+                new Object() {
+                }
                         .getClass()
                         .getEnclosingMethod()
                         .getName()
@@ -269,6 +271,26 @@ public class ProcessCpuTrackUtilsTest {
         Assert.assertEquals(3, stat.cstime);
         long jiffies = stat.utime + stat.stime + stat.cutime + stat.cstime;
         Assert.assertEquals(22 + 2 + 33 + 3, jiffies);
+
+        sample = "10966 (terycanary test) S 699 699 0 0 -1 1077952832 6187 0 0 0 22 2 33 3";
+        stat = parseJiffiesInfoWithBuffer(sample.getBytes());
+        Assert.assertEquals("terycanary test", stat.comm);
+        Assert.assertEquals(22, stat.utime);
+        Assert.assertEquals(2, stat.stime);
+        Assert.assertEquals(33, stat.cutime);
+        Assert.assertEquals(3, stat.cstime);
+        jiffies = stat.utime + stat.stime + stat.cutime + stat.cstime;
+        Assert.assertEquals(22 + 2 + 33 + 3, jiffies);
+
+        sample = "10966 (tery canary test) S 699 699 0 0 -1 1077952832 6187 0 0 0 22 2 33 3";
+        stat = parseJiffiesInfoWithBuffer(sample.getBytes());
+        Assert.assertEquals("tery canary test", stat.comm);
+        Assert.assertEquals(22, stat.utime);
+        Assert.assertEquals(2, stat.stime);
+        Assert.assertEquals(33, stat.cutime);
+        Assert.assertEquals(3, stat.cstime);
+        jiffies = stat.utime + stat.stime + stat.cutime + stat.cstime;
+        Assert.assertEquals(22 + 2 + 33 + 3, jiffies);
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -337,32 +359,27 @@ public class ProcessCpuTrackUtilsTest {
         Assert.fail("TIME CONSUMED: " + timeConsumed1 + " vs " + timeConsumed2);
     }
 
-    public static class ProcStatInfo {
-        @Nullable
-        public String comm;
-        public long utime = -1;
-        public long stime = -1;
-        public long cutime = -1;
-        public long cstime = -1;
-    }
 
     static ProcStatInfo parseJiffiesInfoWithSplitsForPath(String path) {
         return parseJiffiesInfoWithSplits(BatteryCanaryUtil.cat(path));
     }
 
     static ProcStatInfo parseJiffiesInfoWithSplits(String cat) {
-        // cat = cat.substring(0, cat.length() / 2);
         ProcStatInfo stat = new ProcStatInfo();
         if (!TextUtils.isEmpty(cat)) {
-            String[] splits = cat.split(" ");
-            String name = splits[1];
-            if (name.startsWith("(")) name = name.substring(1);
-            if (name.endsWith(")")) name = name.substring(0, name.length() - 1);
-            stat.comm = name;
-            stat.utime = MatrixUtil.parseLong(splits[13], 0);
-            stat.stime = MatrixUtil.parseLong(splits[14], 0);
-            stat.cutime = MatrixUtil.parseLong(splits[15], 0);
-            stat.cstime = MatrixUtil.parseLong(splits[16], 0);
+            int index = cat.indexOf(")");
+            if (index <= 0) throw new IllegalStateException(cat + " has not ')'");
+            String prefix = cat.substring(0, index);
+            int indexBgn = prefix.indexOf("(") + "(".length();
+            stat.comm = prefix.substring(indexBgn, index);
+
+            String suffix = cat.substring(index + ")".length());
+            String[] splits = suffix.split(" ");
+
+            stat.utime = MatrixUtil.parseLong(splits[12], 0);
+            stat.stime = MatrixUtil.parseLong(splits[13], 0);
+            stat.cutime = MatrixUtil.parseLong(splits[14], 0);
+            stat.cstime = MatrixUtil.parseLong(splits[15], 0);
         }
         return stat;
     }
@@ -410,7 +427,7 @@ public class ProcessCpuTrackUtilsTest {
                 sb.append(character);
                 if (' ' == character) {
                     spaceCount++;
-                    if (spaceCount > 16) {
+                    if (spaceCount > 20) {
                         break;
                     }
                 }
@@ -429,120 +446,7 @@ public class ProcessCpuTrackUtilsTest {
 
 
     static ProcStatInfo parseJiffiesInfoWithBuffer(byte[] statBuffer) {
-        /*
-         * 样本:
-         * 10966 (terycanary.test) S 699 699 0 0 -1 1077952832 6187 0 0 0 22 2 0 0 20 0 17 0 9087400 5414273024
-         *  24109 18446744073709551615 421814448128 421814472944 549131058960 0 0 0 4612 1 1073775864
-         *  1 0 0 17 7 0 0 0 0 0 421814476800 421814478232 422247952384 549131060923 549131061022 549131061022
-         *  549131063262 0
-         *
-         * 字段:
-         * - pid:  进程ID.
-         * - comm: task_struct结构体的进程名
-         * - state: 进程状态, 此处为S
-         * - ppid: 父进程ID （父进程是指通过fork方式, 通过clone并非父进程）
-         * - pgrp: 进程组ID
-         * - session: 进程会话组ID
-         * - tty_nr: 当前进程的tty终点设备号
-         * - tpgid: 控制进程终端的前台进程号
-         * - flags: 进程标识位, 定义在include/linux/sched.h中的PF_*, 此处等于1077952832
-         * - minflt:  次要缺页中断的次数, 即无需从磁盘加载内存页. 比如COW和匿名页
-         * - cminflt: 当前进程等待子进程的minflt
-         * - majflt: 主要缺页中断的次数, 需要从磁盘加载内存页. 比如map文件
-         * - majflt: 当前进程等待子进程的majflt
-         * - utime: 该进程处于用户态的时间, 单位jiffies, 此处等于166114
-         * - stime: 该进程处于内核态的时间, 单位jiffies, 此处等于129684
-         * - cutime: 当前进程等待子进程的utime
-         * - cstime: 当前进程等待子进程的utime
-         * - priority: 进程优先级, 此次等于10.
-         * - nice: nice值, 取值范围[19, -20], 此处等于-10
-         * - num_threads: 线程个数, 此处等于221
-         * - itrealvalue: 该字段已废弃, 恒等于0
-         * - starttime: 自系统启动后的进程创建时间, 单位jiffies, 此处等于2284
-         * - vsize: 进程的虚拟内存大小, 单位为bytes
-         * - rss: 进程独占内存+共享库, 单位pages, 此处等于93087
-         * - rsslim: rss大小上限
-         *
-         * 说明:
-         * 第10~17行主要是随着时间而改变的量；
-         * 内核时间单位, sysconf(_SC_CLK_TCK)一般地定义为jiffies(一般地等于10ms)
-         * starttime: 此值单位为jiffies, 结合/proc/stat的btime, 可知道每一个线程启动的时间点
-         * 1500827856 + 2284/100 = 1500827856, 转换成北京时间为2017/7/24 0:37:58
-         * 第四行数据很少使用,只说一下该行第7至9个数的含义:
-         * signal: 即将要处理的信号, 十进制, 此处等于6660
-         * blocked: 阻塞的信号, 十进制
-         * sigignore: 被忽略的信号, 十进制, 此处等于36088
-         */
-
-        ProcStatInfo stat = new ProcStatInfo();
-        int statBytes = statBuffer.length;
-        for (int i = 0, spaceIdx = 0; i < statBytes; ) {
-            if (Character.isSpaceChar(statBuffer[i])) {
-                spaceIdx++;
-                i++;
-                continue;
-            }
-
-            switch (spaceIdx) {
-                case 1: { // read comm (thread name)
-                    int readIdx = i, window = 0;
-                    // seek next space
-                    // noinspection StatementWithEmptyBody
-                    for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
-                    if ('(' == statBuffer[readIdx]) {
-                        readIdx++;
-                        window--;
-                    }
-                    if (')' == statBuffer[readIdx + window - 1]) {
-                        window--;
-                    }
-                    if (window > 0) {
-                        stat.comm = new String(statBuffer, readIdx, window);
-                    }
-                    break;
-                }
-
-                case 13: { // utime
-                    int readIdx = i, window = 0;
-                    // seek next space
-                    // noinspection StatementWithEmptyBody
-                    for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
-                    String num = new String(statBuffer, readIdx, window);
-                    stat.utime = MatrixUtil.parseLong(num, 0);
-                    break;
-                }
-                case 14: { // stime
-                    int readIdx = i, window = 0;
-                    // seek next space
-                    // noinspection StatementWithEmptyBody
-                    for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
-                    String num = new String(statBuffer, readIdx, window);
-                    stat.stime = MatrixUtil.parseLong(num, 0);
-                    break;
-                }
-                case 15: { // cutime
-                    int readIdx = i, window = 0;
-                    // seek next space
-                    // noinspection StatementWithEmptyBody
-                    for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
-                    String num = new String(statBuffer, readIdx, window);
-                    stat.cutime = MatrixUtil.parseLong(num, 0);
-                    break;
-                }
-                case 16: { // cstime
-                    int readIdx = i, window = 0;
-                    // seek next space
-                    // noinspection StatementWithEmptyBody
-                    for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
-                    String num = new String(statBuffer, readIdx, window);
-                    stat.cstime = MatrixUtil.parseLong(num, 0);
-                    break;
-                }
-
-                default:
-                    i++;
-            }
-        }
+        ProcStatInfo stat = ProcStatInfo.parseJiffiesInfoWithBuffer(statBuffer);
 
         Assert.assertNotNull(stat.comm);
         Assert.assertTrue(stat.utime >= 0);
--- matrix/matrix-android/matrix-battery-canary/src/main/java/com/tencent/matrix/batterycanary/utils/BatteryCanaryUtil.java
+++ matrix/matrix-android/matrix-battery-canary/src/main/java/com/tencent/matrix/batterycanary/utils/BatteryCanaryUtil.java
@@ -28,15 +28,19 @@ import android.os.PowerManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
+import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
 import android.view.Display;
 
 import com.tencent.matrix.Matrix;
 import com.tencent.matrix.batterycanary.BatteryMonitorPlugin;
 import com.tencent.matrix.util.MatrixLog;
+import com.tencent.matrix.util.MatrixUtil;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -278,5 +282,169 @@ public final class BatteryCanaryUtil {
         } catch (Throwable ignored) {
         }
         return list;
+    }
+
+
+    @SuppressWarnings("SpellCheckingInspection")
+    public static class ProcStatInfo {
+        private static final byte[] sBuffer = new byte[128];
+
+        public String comm = "";
+        public long utime = -1;
+        public long stime = -1;
+        public long cutime = -1;
+        public long cstime = -1;
+
+        ProcStatInfo() {}
+
+        @Nullable
+        public static ProcStatInfo parseJiffies(String path) {
+            try {
+                return parseJiffiesInfoWithBufferForPath(path, sBuffer);
+            } catch (Exception e) {
+                MatrixLog.printErrStackTrace(TAG, e, "#parseJiffies fail");
+                return null;
+            }
+        }
+
+        public static ProcStatInfo parseJiffiesInfoWithBufferForPath(String path, byte[] buffer) {
+            File file = new File(path);
+            if (!file.exists()) {
+                return null;
+            }
+
+            int readBytes;
+            try (FileInputStream fis = new FileInputStream(file)) {
+                readBytes = fis.read(buffer);
+            } catch (IOException e) {
+                MatrixLog.printErrStackTrace(TAG, e, "read buffer from file fail");
+                readBytes = -1;
+            }
+            if (readBytes <= 0) {
+                return null;
+            }
+
+            return parseJiffiesInfoWithBuffer(buffer);
+        }
+
+        @VisibleForTesting
+        static ProcStatInfo parseJiffiesInfoWithBuffer(byte[] statBuffer) {
+            /*
+             * 样本:
+             * 10966 (terycanary.test) S 699 699 0 0 -1 1077952832 6187 0 0 0 22 2 0 0 20 0 17 0 9087400 5414273024
+             *  24109 18446744073709551615 421814448128 421814472944 549131058960 0 0 0 4612 1 1073775864
+             *  1 0 0 17 7 0 0 0 0 0 421814476800 421814478232 422247952384 549131060923 549131061022 549131061022
+             *  549131063262 0
+             *
+             * 字段:
+             * - pid:  进程ID.
+             * - comm: task_struct结构体的进程名
+             * - state: 进程状态, 此处为S
+             * - ppid: 父进程ID （父进程是指通过fork方式, 通过clone并非父进程）
+             * - pgrp: 进程组ID
+             * - session: 进程会话组ID
+             * - tty_nr: 当前进程的tty终点设备号
+             * - tpgid: 控制进程终端的前台进程号
+             * - flags: 进程标识位, 定义在include/linux/sched.h中的PF_*, 此处等于1077952832
+             * - minflt:  次要缺页中断的次数, 即无需从磁盘加载内存页. 比如COW和匿名页
+             * - cminflt: 当前进程等待子进程的minflt
+             * - majflt: 主要缺页中断的次数, 需要从磁盘加载内存页. 比如map文件
+             * - majflt: 当前进程等待子进程的majflt
+             * - utime: 该进程处于用户态的时间, 单位jiffies, 此处等于166114
+             * - stime: 该进程处于内核态的时间, 单位jiffies, 此处等于129684
+             * - cutime: 当前进程等待子进程的utime
+             * - cstime: 当前进程等待子进程的utime
+             * - priority: 进程优先级, 此次等于10.
+             * - nice: nice值, 取值范围[19, -20], 此处等于-10
+             * - num_threads: 线程个数, 此处等于221
+             * - itrealvalue: 该字段已废弃, 恒等于0
+             * - starttime: 自系统启动后的进程创建时间, 单位jiffies, 此处等于2284
+             * - vsize: 进程的虚拟内存大小, 单位为bytes
+             * - rss: 进程独占内存+共享库, 单位pages, 此处等于93087
+             * - rsslim: rss大小上限
+             *
+             * 说明:
+             * 第10~17行主要是随着时间而改变的量；
+             * 内核时间单位, sysconf(_SC_CLK_TCK)一般地定义为jiffies(一般地等于10ms)
+             * starttime: 此值单位为jiffies, 结合/proc/stat的btime, 可知道每一个线程启动的时间点
+             * 1500827856 + 2284/100 = 1500827856, 转换成北京时间为2017/7/24 0:37:58
+             * 第四行数据很少使用,只说一下该行第7至9个数的含义:
+             * signal: 即将要处理的信号, 十进制, 此处等于6660
+             * blocked: 阻塞的信号, 十进制
+             * sigignore: 被忽略的信号, 十进制, 此处等于36088
+             */
+
+            ProcStatInfo stat = new ProcStatInfo();
+            int statBytes = statBuffer.length;
+            for (int i = 0, spaceIdx = 0; i < statBytes; ) {
+                if (Character.isSpaceChar(statBuffer[i])) {
+                    spaceIdx++;
+                    i++;
+                    continue;
+                }
+
+                switch (spaceIdx) {
+                    case 1: { // read comm (thread name)
+                        int readIdx = i, window = 0;
+                        // seek end symobl of comm: ')'
+                        // noinspection StatementWithEmptyBody
+                        for (; i < statBytes && ')' != statBuffer[i]; i++, window++) ;
+                        if ('(' == statBuffer[readIdx]) {
+                            readIdx++;
+                            window--;
+                        }
+                        if (')' == statBuffer[readIdx + window - 1]) {
+                            window--;
+                        }
+                        if (window > 0) {
+                            stat.comm = new String(statBuffer, readIdx, window);
+                        }
+                        spaceIdx = 2;
+                        break;
+                    }
+
+                    case 14: { // utime
+                        int readIdx = i, window = 0;
+                        // seek next space
+                        // noinspection StatementWithEmptyBody
+                        for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
+                        String num = new String(statBuffer, readIdx, window);
+                        stat.utime = MatrixUtil.parseLong(num, 0);
+                        break;
+                    }
+                    case 15: { // stime
+                        int readIdx = i, window = 0;
+                        // seek next space
+                        // noinspection StatementWithEmptyBody
+                        for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
+                        String num = new String(statBuffer, readIdx, window);
+                        stat.stime = MatrixUtil.parseLong(num, 0);
+                        break;
+                    }
+                    case 16: { // cutime
+                        int readIdx = i, window = 0;
+                        // seek next space
+                        // noinspection StatementWithEmptyBody
+                        for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
+                        String num = new String(statBuffer, readIdx, window);
+                        stat.cutime = MatrixUtil.parseLong(num, 0);
+                        break;
+                    }
+                    case 17: { // cstime
+                        int readIdx = i, window = 0;
+                        // seek next space
+                        // noinspection StatementWithEmptyBody
+                        for (; i < statBytes && !Character.isSpaceChar(statBuffer[i]); i++, window++) ;
+                        String num = new String(statBuffer, readIdx, window);
+                        stat.cstime = MatrixUtil.parseLong(num, 0);
+                        break;
+                    }
+
+                    default:
+                        i++;
+                }
+            }
+            return stat;
+        }
     }
 }
