5dd6f0a52dce2c61774c9ef7abe2f5253dec8689
陈艺玲 <feiling.cyl@autonavi.com>
2021-03-09 16:33:02 +0800

    fix issue #421 StartupTracer不兼容AndroidX问题

--- matrix/matrix-android/matrix-gradle-plugin/src/main/java/com/tencent/matrix/trace/TraceBuildConstants.java
+++ matrix/matrix-android/matrix-gradle-plugin/src/main/java/com/tencent/matrix/trace/TraceBuildConstants.java
@@ -30,6 +30,7 @@ public class TraceBuildConstants {
     public final static String MATRIX_TRACE_ACTIVITY_CLASS = "android/app/Activity";
     public final static String MATRIX_TRACE_V7_ACTIVITY_CLASS = "android/support/v7/app/AppCompatActivity";
     public final static String MATRIX_TRACE_V4_ACTIVITY_CLASS = "android/support/v4/app/FragmentActivity";
+    public final static String MATRIX_TRACE_ANDROIDX_ACTIVITY_CLASS = "androidx/appcompat/app/AppCompatActivity";
     public final static String MATRIX_TRACE_APPLICATION_CLASS = "android/app/Application";
     public final static String MATRIX_TRACE_METHOD_BEAT_CLASS = "com/tencent/matrix/trace/core/AppMethodBeat";
     public final static String MATRIX_TRACE_ON_WINDOW_FOCUS_METHOD_ARGS = "(Z)V";
