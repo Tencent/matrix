f51c7d034ef307ca81b72f74b104eb392a078e25
carlguo <carlguo@tencent.com>
2021-05-28 19:51:09 +0800

    Add License.

--- matrix/matrix-android/matrix-backtrace/src/main/cpp/libwechatbacktrace/DebugDexFiles.cpp
+++ matrix/matrix-android/matrix-backtrace/src/main/cpp/libwechatbacktrace/DebugDexFiles.cpp
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Tencent is pleased to support the open source community by making wechat-matrix available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the BSD 3-Clause License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://opensource.org/licenses/BSD-3-Clause
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -108,6 +108,7 @@ namespace wechat_backtrace {
 
     BACKTRACE_EXPORT DebugDexFiles::DebugDexFiles(std::shared_ptr<Memory> &memory) : DebugGlobal(memory) {
         SetArch(unwindstack::Regs::CurrentArch());
+        search_libs_.push_back("libart.so");
     }
 
     BACKTRACE_EXPORT DebugDexFiles::~DebugDexFiles() {}
