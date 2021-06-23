5e4d36d884f8d9c4b7aab63b33cf3db1c4a8ab10
kaedexie <kaedexie@tencent.com>
2021-06-23 15:37:08 +0800

    Update battery metrics test

--- matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/BatteryMetricsTest.java
+++ matrix/matrix-android/matrix-battery-canary/src/androidTest/java/com/tencent/matrix/batterycanary/BatteryMetricsTest.java
@@ -17,6 +17,11 @@
 package com.tencent.matrix.batterycanary;
 
 import android.content.Context;
+import android.os.Process;
+import android.os.SystemClock;
+import android.util.Log;
+
+import com.tencent.matrix.batterycanary.utils.ProcStatUtil;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -26,6 +31,7 @@ import org.junit.runner.RunWith;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Stack;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -33,7 +39,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 
 @RunWith(AndroidJUnit4.class)
 public class BatteryMetricsTest {
-    static final String TAG = "Matrix.test.foo";
+    static final String TAG = "Matrix.test.metrics";
 
     Context mContext;
 
@@ -52,5 +58,90 @@ public class BatteryMetricsTest {
         Constructor<?> constructor = clazz.getConstructor(Context.class);
         Object obj = constructor.newInstance(mContext);
         Assert.assertNotNull(obj);
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Test
+    public void testJiffiesConsumption() {
+        long wallTimeBgn = System.currentTimeMillis();
+        long upTimeBgn = SystemClock.uptimeMillis();
+        long threadTimeBgn = SystemClock.currentThreadTimeMillis();
+
+        ProcStatUtil.ProcStat procStatBgn = ProcStatUtil.of(Process.myPid(), Process.myTid());
+        long threadJiffiesBgn = procStatBgn.getJiffies();
+        long threadUtimeBgn = procStatBgn.utime;
+        long threadStimeBgn = procStatBgn.stime;
+        long threadCutimeBgn = procStatBgn.cutime;
+        long threadCstimeBgn = procStatBgn.cstime;
+
+        CpuConsumption.inc(Integer.MAX_VALUE);
+
+        ProcStatUtil.ProcStat procStatEnd = ProcStatUtil.of(Process.myPid(), Process.myTid());
+        StringBuilder sb = new StringBuilder().append("wallTimeDiff = ").append(System.currentTimeMillis() - wallTimeBgn)
+                .append("\nupTimeDiff = ").append(SystemClock.uptimeMillis() - upTimeBgn)
+                .append("\nthreadTimeDiff = ").append(SystemClock.currentThreadTimeMillis() - threadTimeBgn)
+                .append("\nthreadJiffiesDiff = ").append(procStatEnd.getJiffies() - threadJiffiesBgn)
+                .append("\nthreadUtimeDiff = ").append(procStatEnd.utime - threadUtimeBgn)
+                .append("\nthreadStimeDiff = ").append(procStatEnd.stime - threadStimeBgn)
+                .append("\nthreadCutimeDiff = ").append(procStatEnd.cutime - threadCutimeBgn)
+                .append("\nthreadCstimeDiff = ").append(procStatEnd.cstime - threadCstimeBgn)
+                .append("\n");
+        Assert.fail(sb.toString());
+    }
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @Test
+    public void testJiffiesConsumption2() {
+        long wallTimeBgn = System.currentTimeMillis();
+        long upTimeBgn = SystemClock.uptimeMillis();
+        long threadTimeBgn = SystemClock.currentThreadTimeMillis();
+
+        ProcStatUtil.ProcStat procStatBgn = ProcStatUtil.of(Process.myPid(), Process.myTid());
+        long threadJiffiesBgn = procStatBgn.getJiffies();
+        long threadUtimeBgn = procStatBgn.utime;
+        long threadStimeBgn = procStatBgn.stime;
+        long threadCutimeBgn = procStatBgn.cutime;
+        long threadCstimeBgn = procStatBgn.cstime;
+
+        CpuConsumption.hanoi(20);
+
+        ProcStatUtil.ProcStat procStatEnd = ProcStatUtil.of(Process.myPid(), Process.myTid());
+        StringBuilder sb = new StringBuilder().append("wallTimeDiff = ").append(System.currentTimeMillis() - wallTimeBgn)
+                .append("\nupTimeDiff = ").append(SystemClock.uptimeMillis() - upTimeBgn)
+                .append("\nthreadTimeDiff = ").append(SystemClock.currentThreadTimeMillis() - threadTimeBgn)
+                .append("\nthreadJiffiesDiff = ").append(procStatEnd.getJiffies() - threadJiffiesBgn)
+                .append("\nthreadUtimeDiff = ").append(procStatEnd.utime - threadUtimeBgn)
+                .append("\nthreadStimeDiff = ").append(procStatEnd.stime - threadStimeBgn)
+                .append("\nthreadCutimeDiff = ").append(procStatEnd.cutime - threadCutimeBgn)
+                .append("\nthreadCstimeDiff = ").append(procStatEnd.cstime - threadCstimeBgn)
+                .append("\n");
+        Assert.fail(sb.toString());
+    }
+
+    public static class CpuConsumption {
+        public static void inc(int num) {
+            for (int i = 0; i < num; i++) {
+            }
+        }
+
+        public static void hanoi(int num) {
+            Stack<Integer> torre1 = new Stack<>();
+            Stack<Integer> torre2 = new Stack<>();
+            Stack<Integer> torre3 = new Stack<>();
+
+            for (int i = num; i >= 1; i--) {
+                torre1.push(i);
+            }
+            ordena(torre1.size(), torre1, torre3, torre2);
+        }
+
+        private static void ordena(int n, Stack<Integer> torre1, Stack<Integer> torre3, Stack<Integer> torre2) {
+            if (n > 0) {
+                ordena(n - 1, torre1, torre2, torre3);
+                torre3.push(torre1.pop());
+                Log.i(TAG, "Torre1: " + torre1 + " Torre 2: " + torre2 + " Torre3: " + torre3);
+                ordena(n - 1, torre2, torre3, torre1);
+            }
+        }
     }
 }
