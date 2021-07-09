00ee4e2105e2f48f6444d526c4325893a0414e82
tomystang <tomystang@tencent.com>
2021-07-09 22:26:38 +0800

    Give up supporting N and N_MR1 in SoLoadMonitor so far.

--- matrix/matrix-android/matrix-hooks/src/main/cpp/common/SoLoadMonitor.cpp
+++ matrix/matrix-android/matrix-hooks/src/main/cpp/common/SoLoadMonitor.cpp
@@ -29,39 +29,34 @@ namespace matrix {
         }
     }
 
-    static void* (*do_dlopen_ge_N)(const char* name, int flags, const void* extinfo, const void* caller_addr) = nullptr;
-    static void* (*do_dlopen_lt_N)(const char* name, int flags, const void* extinfo) = nullptr;
-
+    static void* (*orig__loader_dlopen)(const char* filename, int flags, const void* caller_addr) = nullptr;
     static void* __loader_dlopen_handler(const char* filename, int flags, const void* caller_addr) {
         std::lock_guard dlopenLock(sDlOpenMutex);
-        void *hLib = do_dlopen_ge_N(filename, flags, nullptr, caller_addr);
+        void *hLib = orig__loader_dlopen(filename, flags, caller_addr);
         if (hLib != nullptr) {
             NotifySoLoad(filename);
         }
         return hLib;
     }
 
+    static void* (*orig__loader_android_dlopen_ext)(const char* filename,
+            int flag, const void* extinfo, const void* caller_addr) = nullptr;
     static void* __loader_android_dlopen_ext_handler(const char* filename,
-                                                     int flag, const void* extinfo, const void* caller_addr) {
+            int flag, const void* extinfo, const void* caller_addr) {
         std::lock_guard dlopenLock(sDlOpenMutex);
-        void *hLib = do_dlopen_ge_N(filename, flag, extinfo, caller_addr);
+        void *hLib = orig__loader_android_dlopen_ext(filename, flag, extinfo, caller_addr);
         if (hLib != nullptr) {
             NotifySoLoad(filename);
         }
         return hLib;
     }
 
+    static void* (*orig_dlopen)(const char* filename, int flags) = nullptr;
     static void* dlopen_handler(const char* filename, int flags) {
         int sdkVer = android_get_device_api_level();
         {
             std::lock_guard dlopenLock(sDlOpenMutex);
-            void *hLib = nullptr;
-            if (sdkVer >= 24) {
-                void* callerAddr = __builtin_return_address(0);
-                hLib = do_dlopen_ge_N(filename, flags, nullptr, callerAddr);
-            } else {
-                hLib = do_dlopen_lt_N(filename, flags, nullptr);
-            }
+            void *hLib = orig_dlopen(filename, flags);
             if (hLib != nullptr) {
                 NotifySoLoad(filename);
             }
@@ -69,17 +64,12 @@ namespace matrix {
         }
     }
 
+    static void* (*orig_android_dlopen_ext)(const char* filename, int flag, const void* extinfo) = nullptr;
     static void* android_dlopen_ext_handler(const char* filename, int flag, const void* extinfo) {
         int sdkVer = android_get_device_api_level();
         {
             std::lock_guard dlopenLock(sDlOpenMutex);
-            void *hLib = nullptr;
-            if (sdkVer >= 24) {
-                void* callerAddr = __builtin_return_address(0);
-                hLib = do_dlopen_ge_N(filename, flag, extinfo, callerAddr);
-            } else {
-                hLib = do_dlopen_lt_N(filename, flag, extinfo);
-            }
+            void *hLib = orig_android_dlopen_ext(filename, flag, extinfo);
             if (hLib != nullptr) {
                 NotifySoLoad(filename);
             }
@@ -102,43 +92,39 @@ namespace matrix {
 
         int sdkVer = android_get_device_api_level();
 
-        {
-            void *hLinker = matrix::SemiDlOpen(LINKER_NAME);
-            if (hLinker == nullptr) {
-                LOGE(LOG_TAG, "Fail to open %s.", LINKER_NAME);
-                return false;
-            }
-            auto hLinkerCloser = MakeScopedCleaner([&hLinker]() {
-                if (hLinker != nullptr) {
-                    matrix::SemiDlClose(hLinker);
-                }
-            });
-
-            if (sdkVer >= 24) {
-                do_dlopen_ge_N = reinterpret_cast<decltype(do_dlopen_ge_N)>(
-                        matrix::SemiDlSym(hLinker, "__dl__Z9do_dlopenPKciPK17android_dlextinfoPKv"));
-                if (UNLIKELY(do_dlopen_ge_N == nullptr)) {
-                    do_dlopen_ge_N = reinterpret_cast<decltype(do_dlopen_ge_N)>(
-                            matrix::SemiDlSym(hLinker, "__dl__Z9do_dlopenPKciPK17android_dlextinfoPv"));
-                }
-                if (UNLIKELY(do_dlopen_ge_N == nullptr)) {
-                    LOGE(LOG_TAG, "Fail to fetch address of do_dlopen.");
-                    return false;
-                }
-            } else {
-                do_dlopen_lt_N = reinterpret_cast<decltype(do_dlopen_lt_N)>(
-                        matrix::SemiDlSym(hLinker, "__dl__Z9do_dlopenPKciPK17android_dlextinfo"));
-                if (UNLIKELY(do_dlopen_lt_N == nullptr)) {
-                    LOGE(LOG_TAG, "Fail to fetch address of do_dlopen.");
-                    return false;
-                }
-            }
+        if (sdkVer == 24 || sdkVer == 25) {
+            LOGE(LOG_TAG, "Does not support N and N_MR1 so far.");
+            return false;
         }
 
+        void *hLinker = matrix::SemiDlOpen(LINKER_NAME);
+        if (hLinker == nullptr) {
+            LOGE(LOG_TAG, "Fail to open %s.", LINKER_NAME);
+            return false;
+        }
+        auto hLinkerCloser = MakeScopedCleaner([&hLinker]() {
+            if (hLinker != nullptr) {
+                matrix::SemiDlClose(hLinker);
+            }
+        });
+
         if (sdkVer >= 26 /* O */) {
+            orig__loader_dlopen = reinterpret_cast<decltype(orig__loader_dlopen)>(
+                    matrix::SemiDlSym(hLinker, "__dl___loader_dlopen"));
+            if (UNLIKELY(orig__loader_dlopen == nullptr)) {
+                LOGE(LOG_TAG, "Fail to find original __loader_dlopen.");
+                return false;
+            }
             if (UNLIKELY(xhook_register(".*\\.so$", "__loader_dlopen",
                     reinterpret_cast<void*>(__loader_dlopen_handler), nullptr) != 0)) {
                 LOGE(LOG_TAG, "Fail to register __loader_dlopen hook handler.");
+                return false;
+            }
+
+            orig__loader_android_dlopen_ext = reinterpret_cast<decltype(orig__loader_android_dlopen_ext)>(
+                    matrix::SemiDlSym(hLinker, "__dl___loader_android_dlopen_ext"));
+            if (UNLIKELY(orig__loader_android_dlopen_ext == nullptr)) {
+                LOGE(LOG_TAG, "Fail to find original __loader_android_dlopen_ext.");
                 return false;
             }
             if (UNLIKELY(xhook_register(".*\\.so$", "__loader_android_dlopen_ext",
@@ -147,9 +133,21 @@ namespace matrix {
                 return false;
             }
         } else {
+            orig_dlopen = reinterpret_cast<decltype(orig_dlopen)>(matrix::SemiDlSym(hLinker, "__dl_dlopen"));
+            if (UNLIKELY(orig_dlopen == nullptr)) {
+                LOGE(LOG_TAG, "Fail to find original dlopen.");
+                return false;
+            }
             if (UNLIKELY(xhook_register(".*\\.so$", "dlopen",
                     reinterpret_cast<void*>(dlopen_handler), nullptr) != 0)) {
                 LOGE(LOG_TAG, "Fail to register dlopen hook handler.");
+                return false;
+            }
+
+            orig_android_dlopen_ext = reinterpret_cast<decltype(orig_android_dlopen_ext)>(
+                    matrix::SemiDlSym(hLinker, "__dl_android_dlopen_ext"));
+            if (UNLIKELY(orig_android_dlopen_ext == nullptr)) {
+                LOGE(LOG_TAG, "Fail to find original android_dlopen_ext.");
                 return false;
             }
             if (UNLIKELY(xhook_register(".*\\.so$", "android_dlopen_ext",
