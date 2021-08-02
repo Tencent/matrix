a5f29dc5c655b65f9d89569952530a04877c586b
tomystang <tomystang@tencent.com>
2021-08-02 17:30:06 +0800

    Make SoLoadMonitor support Android N.

--- matrix/matrix-android/matrix-hooks/src/main/cpp/common/SoLoadMonitor.cpp
+++ matrix/matrix-android/matrix-hooks/src/main/cpp/common/SoLoadMonitor.cpp
@@ -130,6 +130,40 @@ namespace matrix {
         }
     }
 
+    static void* (*orig_do_dlopen_N)(const char* filename, int flag, const void* extinfo, const void* calller) = nullptr;
+
+    static TLSVAR bool android_dlopen_ext_N_handler_reentered = false;
+    static void* android_dlopen_ext_N_handler(const char* filename, int flags, const void* extinfo) {
+        const void* caller_addr = __builtin_return_address(0);
+        auto implFn = [](const DlOpenArgs* args) {
+            return orig_do_dlopen_N(args->filename, args->flags, args->extinfo, args->caller_addr);
+        };
+        DlOpenArgs args = { .filename = filename, .flags = flags, .extinfo = extinfo, .caller_addr = caller_addr };
+        if (UNLIKELY(android_dlopen_ext_N_handler_reentered)) {
+            return HandleDlOpenCommon(implFn, &args);
+        } else {
+            MARK_REENTERED(android_dlopen_ext_N_handler_reentered);
+            WAIT_ON_COND(sDlOpenBlockerMutex, sDlOpenBlocker, sDlOpenPaused);
+            return HandleDlOpenCommon(implFn, &args);
+        }
+    }
+
+    static TLSVAR bool dlopen_N_handler_reentered = false;
+    static void* dlopen_N_handler(const char* filename, int flags) {
+        const void* caller_addr = __builtin_return_address(0);
+        auto implFn = [](const DlOpenArgs* args) {
+            return orig_do_dlopen_N(args->filename, args->flags, nullptr, args->caller_addr);
+        };
+        DlOpenArgs args = { .filename = filename, .flags = flags, .caller_addr = caller_addr };
+        if (UNLIKELY(dlopen_N_handler_reentered)) {
+            return HandleDlOpenCommon(implFn, &args);
+        } else {
+            MARK_REENTERED(dlopen_N_handler_reentered);
+            WAIT_ON_COND(sDlOpenBlockerMutex, sDlOpenBlocker, sDlOpenPaused);
+            return HandleDlOpenCommon(implFn, &args);
+        }
+    }
+
     static TLSVAR bool android_dlopen_ext_handler_reentered = false;
     static void* (*orig_android_dlopen_ext)(const char* filename, int flag, const void* extinfo) = nullptr;
     static void* android_dlopen_ext_handler(const char* filename, int flags, const void* extinfo) {
@@ -161,11 +195,6 @@ namespace matrix {
         }
 
         int sdkVer = android_get_device_api_level();
-
-        if (sdkVer == 24 || sdkVer == 25) {
-            LOGE(LOG_TAG, "Does not support N and N_MR1 so far.");
-            return false;
-        }
 
         void *hLinker = semi_dlopen(LINKER_NAME);
         if (hLinker == nullptr) {
@@ -205,9 +234,28 @@ namespace matrix {
                 LOGE(LOG_TAG, "Fail to find original __loader_android_dlopen_ext.");
                 return false;
             }
-            if (UNLIKELY(xhook_grouped_register(HOOK_REQUEST_GROUPID_DLOPEN_MON, ".*\\.so$", "__loader_android_dlopen_ext",
-                                                reinterpret_cast<void*>(__loader_android_dlopen_ext_handler), nullptr) != 0)) {
+            if (UNLIKELY(
+                    xhook_grouped_register(HOOK_REQUEST_GROUPID_DLOPEN_MON, ".*\\.so$", "__loader_android_dlopen_ext",
+                                           reinterpret_cast<void*>(__loader_android_dlopen_ext_handler), nullptr) !=
+                    0)) {
                 LOGE(LOG_TAG, "Fail to register __loader_android_dlopen_ext hook handler.");
+                return false;
+            }
+        } else if (sdkVer >= 24 && sdkVer <= 25 /* N */) {
+            orig_do_dlopen_N = reinterpret_cast<decltype(orig_do_dlopen_N)>(
+                    semi_dlsym(hLinker, "__dl__Z9do_dlopenPKciPK17android_dlextinfoPv"));
+            if (UNLIKELY(orig_do_dlopen_N == nullptr)) {
+                LOGE(LOG_TAG, "Fail to find original do_dlopen.");
+                return false;
+            }
+            if (UNLIKELY(xhook_grouped_register(HOOK_REQUEST_GROUPID_DLOPEN_MON, ".*\\.so$", "android_dlopen_ext",
+                                                reinterpret_cast<void*>(android_dlopen_ext_N_handler), nullptr) != 0)) {
+                LOGE(LOG_TAG, "Fail to register android_dlopen_ext hook handler.");
+                return false;
+            }
+            if (UNLIKELY(xhook_grouped_register(HOOK_REQUEST_GROUPID_DLOPEN_MON, ".*\\.so$", "dlopen",
+                                                reinterpret_cast<void*>(dlopen_N_handler), nullptr) != 0)) {
+                LOGE(LOG_TAG, "Fail to register dlopen hook handler.");
                 return false;
             }
         } else {
