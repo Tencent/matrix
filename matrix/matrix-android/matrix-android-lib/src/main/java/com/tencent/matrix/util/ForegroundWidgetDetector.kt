14ec9ac4478eb2b132250ce7b504573ccd2d55b6
yvesluo <yvesluo@tencent.com>
2021-11-25 20:42:12 +0800

    lifecycle: fix floating view detection

--- matrix/matrix-android/matrix-android-lib/src/main/java/com/tencent/matrix/util/ForegroundWidgetDetector.kt
+++ matrix/matrix-android/matrix-android-lib/src/main/java/com/tencent/matrix/util/ForegroundWidgetDetector.kt
@@ -4,16 +4,18 @@ import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.Context
 import android.os.Process
+import android.view.View
 import com.tencent.matrix.Matrix
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by Yves on 2021/11/23
  */
+@SuppressLint("PrivateApi", "DiscouragedPrivateApi")
 object ForegroundWidgetDetector {
     private const val TAG = "Matrix.ForegroundWidgetDetector"
 
-    @JvmStatic
     private val am: ActivityManager by lazy {
         Matrix.with().application.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
     }
@@ -28,18 +30,34 @@ object ForegroundWidgetDetector {
             }
     }
 
+    private val WindowManagerGlobal_mRoots by lazy {
+        safeLetOrNull(TAG) {
+            Class.forName("android.view.WindowManagerGlobal").let {
+                val instance = ReflectUtils.invoke<Any>(it, "getInstance", null)
+                ReflectUtils.get<ArrayList<*>>(it, "mRoots", instance)
+            }
+        }
+    }
+
+    private val field_ViewRootImpl_mView by lazy {
+        safeLetOrNull(TAG) {
+            ReflectFiled<View>(Class.forName("android.view.ViewRootImpl"), "mView")
+        }
+    }
+
     @JvmStatic
     @SuppressLint("PrivateApi", "DiscouragedPrivateApi")
     fun hasFloatingView() = safeLet(TAG, log = true, defVal = false) {
-        val clazzWindowManagerGlobal = Class.forName("android.view.WindowManagerGlobal")
-        val methodGetInstance = clazzWindowManagerGlobal.getDeclaredMethod("getInstance")
-            .apply { isAccessible = true }
-        val objGlobal = methodGetInstance.invoke(null)
-        val fieldRoots = clazzWindowManagerGlobal.getDeclaredField("mRoots")
-            .apply { isAccessible = true }
-        val mRoots = fieldRoots.get(objGlobal) as ArrayList<*>
-        val clazzViewRootImpl = Class.forName("android.view.ViewRootImpl")
-        val fieldView = clazzViewRootImpl.getDeclaredField("mView")
-        return@safeLet mRoots.filter { fieldView.get(it) != null }.size > 1
+        if (WindowManagerGlobal_mRoots == null) {
+            MatrixLog.e(TAG, "WindowManagerGlobal_mRoots not found")
+            return@safeLet false
+        }
+        if (field_ViewRootImpl_mView == null) {
+            MatrixLog.e(TAG, "field_ViewRootImpl_mView not found")
+            return@safeLet false
+        }
+        return@safeLet WindowManagerGlobal_mRoots!!.any {
+            View.VISIBLE == field_ViewRootImpl_mView!!.get(it)?.visibility
+        }
     }
 }
