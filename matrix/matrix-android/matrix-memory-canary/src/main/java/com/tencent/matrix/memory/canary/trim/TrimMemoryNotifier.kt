4c197f193aac8424b965d8b5874f76b19ae82bb9
yvesluo <yvesluo@tencent.com>
2022-05-16 10:44:09 +0800

    memory-canary: fix trim memory index out of bounds error

--- matrix/matrix-android/matrix-memory-canary/src/main/java/com/tencent/matrix/memory/canary/trim/TrimMemoryNotifier.kt
+++ matrix/matrix-android/matrix-memory-canary/src/main/java/com/tencent/matrix/memory/canary/trim/TrimMemoryNotifier.kt
@@ -88,7 +88,7 @@ object TrimMemoryNotifier {
                     runningHandler.postDelayed(this@TrimTask, delay)
                     MatrixLog.i(
                         TAG,
-                        "[$name] trim delay[${delayIndex+1}/${config.delayMillis.size}] $delay"
+                        "[$name] trim delay[${delayIndex + 1}/${config.delayMillis.size}] $delay"
                     )
                 }
 
@@ -100,18 +100,18 @@ object TrimMemoryNotifier {
         }
 
         override fun run() {
-            MatrixLog.i(
-                TAG,
-                "[$name] trim timeout [${delayIndex+1}/${config.delayMillis.size}] ${config.delayMillis[delayIndex]}"
-            )
-            delayIndex++
-            trimCallback.backgroundTrim()
             if (delayIndex < config.delayMillis.size) {
+                MatrixLog.i(
+                    TAG,
+                    "[$name] trim timeout [${delayIndex + 1}/${config.delayMillis.size}] ${config.delayMillis[delayIndex]}"
+                )
+                delayIndex++
+                trimCallback.backgroundTrim()
                 val delay = config.delayMillis[delayIndex]
                 runningHandler.postDelayed(this, delay)
                 MatrixLog.i(
                     TAG,
-                    "[$name] trim delay[${delayIndex+1}/${config.delayMillis.size}] $delay"
+                    "[$name] trim delay[${delayIndex + 1}/${config.delayMillis.size}] $delay"
                 )
             }
         }
