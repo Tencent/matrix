c029184027fd8216c8e02c620842844200ceb1ce
yvesluo <yvesluo@tencent.com>
2022-07-26 17:32:44 +0800

    mallctl: remove useless code

---
+++ matrix/matrix-android/matrix-mallctl/src/main/java/com/tencent/matrix/mallctl/MallCtl.java
@@ -0,0 +1,56 @@
+/*
+ * Tencent is pleased to support the open source community by making wechat-matrix available.
+ * Copyright (C) 2021 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.matrix.mallctl;
+
+
+import androidx.annotation.Keep;
+
+import com.tencent.matrix.util.MatrixLog;
+
+/**
+ * Created by Yves on 2020/7/15
+ */
+public class MallCtl {
+    private static final String TAG = "Matrix.JeCtl";
+
+    private static boolean initialized = false;
+
+    static {
+        try {
+            System.loadLibrary("matrix-mallctl");
+            initNative();
+            initialized = true;
+        } catch (Throwable e) {
+            MatrixLog.printErrStackTrace(TAG, e, "");
+        }
+    }
+
+    public synchronized static String jeVersion() {
+        if (!initialized) {
+            MatrixLog.e(TAG, "JeCtl init failed! check if so exists");
+            return "VER_UNKNOWN";
+        }
+
+        return getVersionNative();
+    }
+
+    @Keep
+    private static native void initNative();
+
+    @Keep
+    private static native String getVersionNative();
+}
